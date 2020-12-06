4674107de63b6d7c7ad06abedd41133f65fa9c97
Bubba Lichvar <bubbathevtog@gmail.com>
2020-12-06 00:30:59 -0500

    making a general push

--- src/advent/day4.ts
+++ src/advent/day4.ts
@@ -10,6 +10,25 @@ export type Passport = {
 	cid?:string,
 }
 
+/**
+ * This retokenizes data by looking ahead to build groups
+ * It was a pain in the ass to realize that it was needed.
+ */
+function ReTokenize(input:string):string[] {
+	let dataByLine = input.split('\n')
+	let dataByGroup:string[] = []
+
+	for (let i=0;i<dataByLine.length; i++) {
+		let group:string[] = []
+		while (dataByLine[i].length != 0){
+			group.push(dataByLine[i])
+			i++
+		}
+			dataByGroup.push(group.join(' '))
+	}
+
+	return dataByGroup
+}
 export function PassportGenerator(input:string):Passport[] {
 	let Passports:Passport[] = []
 	let tokenizedData = ReTokenize(input)
@@ -101,37 +120,97 @@ export function PassportsValidator(passports:Passport[]): Passport[] {
 
 export function PassportValidator(passport:Passport): boolean {
 	let issues:number = 0
-	issues |= passport.ecl ? 0:1
-	issues |= passport.pid ? 0:1
-	issues |= passport.hcl ? 0:1
-	issues |= passport.byr ? 0:1
-	issues |= passport.iyr ? 0:1
-	issues |= passport.eyr ? 0:1
-	issues |= passport.hgt ? 0:1
+	//issues |= isECLValid(passport.ecl) ? 0:1
+	//issues |= isPIDValid(passport.pid) ? 0:1
+	//issues |= isHCLValid(passport.hcl) ? 0:1
+	//issues |= isBYRValid(passport.byr) ? 0:1
+	issues |= isIYRValid(passport.iyr) ? 0:1
+	//issues |= isERYValid(passport.eyr) ? 0:1
+	//issues |= isHGTValid(passport.hgt) ? 0:1
 
 	if (issues > 0) {
-		console.log('Bad passport: ', passport)
+		console.log('Bad passport: ', passport.iyr)
 	}
 
 	return (issues > 0 ? false : true)
 }
 
 /**
- * This retokenizes data by looking ahead to build groups
- * It was a pain in the ass to realize that it was needed.
- */
-function ReTokenize(input:string):string[] {
-	let dataByLine = input.split('\n')
-	let dataByGroup:string[] = []
+	* ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+	* @returns boolean - True if valid.
+	*/
+function isECLValid(input:string):boolean {
+	let validVals = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+	return validVals.includes(input)
+}
 
-	for (let i=0;i<dataByLine.length; i++) {
-		let group:string[] = []
-		while (dataByLine[i].length != 0){
-			group.push(dataByLine[i])
-			i++
-		}
-			dataByGroup.push(group.join(' '))
+/**
+	* pid (Passport ID) - a nine-digit number, including leading zeroes.`
+	* @returns boolean - True if valid.
+	*/
+function isPIDValid(input:string):boolean {
+	if (!input) { return false }
+	return (input.match(/[0-9]{9}/) ? true:false)
+}
+
+/**
+	* hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+	* @returns boolean - True if valid.
+	*/
+function isHCLValid(input:string):boolean {
+	if (!input) { return false }
+	return (input.match(/#[a-fA-F0-9]{6}/) ? true:false)
+}
+
+/**
+	* byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	* @returns boolean - True if valid.
+	*/
+function isBYRValid(input:string):boolean {
+	if (!input) { return false }
+	if (input.length != 4) { return false }
+	let year = parseInt(input)
+	return ((year >= 1920) && (year <= 2002))
+}
+
+/**
+	* iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	* @returns boolean - True if valid.
+	*/
+function isIYRValid(input:string):boolean {
+	if (!input) { return false }
+	if (input.length != 4) { return false }
+	let year = parseInt(input)
+	return ((year >= 2010) && (year <= 2020))
+}
+
+/**
+	* eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	* @returns boolean - True if valid.
+	*/
+function isERYValid(input:string):boolean {
+	if (!input) { return false }
+	if (input.length != 4) { return false }
+	let year = parseInt(input)
+	return ((year >= 2020) && (year <= 2030))
+}
+
+/**
+	* hgt (Height) - a number followed by either cm or in:
+  *   If cm, the number must be at least 150 and at most 193.
+  *   If in, the number must be at least 59 and at most 76.
+	* @returns boolean - True if valid.
+	*/
+function isHGTValid(input:string):boolean {
+	if (!input) { return false }
+	let units = input.slice(-2)
+
+	let number = parseInt(input)
+	if (units === 'cm') {
+		return ((number > 150) && (number < 193))
+	} else if (units === 'in') {
+		return ((number > 59) && (number < 76))
+	} else {
+		return false
 	}
-
-	return dataByGroup
 }
